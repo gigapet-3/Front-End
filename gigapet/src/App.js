37bda25d4085ffca2870c99af9a07ae5b5b517b2
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-01-31 17:08:50 -0800

    axiosWithAuth, PrivateRoute

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+      <Login />
+
     </div>
   );
 }
---
+++ gigapet/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Login = () => {
+
+
+    const handleSubmit = e => {
+        //this will be axioswithauth, let React 2 people know when you hit this point ;)
+    }
+    return (
+        <div>I am login</div>
+    );
+}
+
+export default Login;
