d6ba4b74c804af6f0326bf5e062f5cb309eaacbd
debauchery1st <trevor.j.martin@gmail.com>
2020-02-01 23:39:47 -0500

    title placeholder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import { Login, Register, Dashboard, NavBar, Start } from "./components";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const location = useLocation();
+  const title = location.pathname.replace("/", "") || "welcome";
   return (
     <div className="App">
       <NavBar />
+      <div className="nav-title text-center">
+        <h4 className="p-2">{title}</h4>
+      </div>
       <Switch>
         <Route exact path="/" component={Start} />
         <Route exact path="/login" component={Login} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,37 +7,39 @@ import "./styles/NavBar.css";
 // example
 const NavBar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <a className="navbar-brand" href="/dashboard">
-        LambdiPet
-      </a>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <a className="navbar-brand" href="/dashboard">
+          LambdiPet
+        </a>
 
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#gigapetNavbarToggler"
-        aria-controls="gigapetNavbarToggler"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="gigapetNavbarToggler">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link text-right" to="/login">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link text-right" to="/register">
-              Register
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#gigapetNavbarToggler"
+          aria-controls="gigapetNavbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="gigapetNavbarToggler">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link text-right" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-right" to="/register">
+                Register
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
