a8595652e46d04258f9e52bef30a80235cf62b85
debauchery1st <trevor.j.martin@gmail.com>
2020-02-03 14:48:45 -0500

    temporary design space

--- src/components/TestSpace.jsx
+++ src/components/TestSpace.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import GigapetCard from "./GigapetCard";
-
+import "./styles/TestSpace.css";
 const TestSpace = props => {
   return (
-    <div>
+    <>
       <h3>testing component</h3>
-      <GigapetCard name="Kathleen" status="is feeling all sorts of hungry" />
-    </div>
+      <div className="test-gigapet-cards">
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+        <GigapetCard name="Kathleen" status="meow" />
+      </div>
+    </>
   );
 };
 
