a49d201596a358459b6399d4a0cdcc351c15b048
debauchery1st <trevor.j.martin@gmail.com>
2020-02-03 14:31:54 -0500

    gigapet card

---
+++ src/components/TestSpace.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GigapetCard from "./GigapetCard";
+
+const TestSpace = props => {
+  return (
+    <div>
+      <h3>testing component</h3>
+      <GigapetCard name="Kathleen" status="is feeling all sorts of hungry" />
+    </div>
+  );
+};
+
+export default TestSpace;
