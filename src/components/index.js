f57705f7f56d1ee0226c0107686d420dd756a7ce
debauchery1st <trevor.j.martin@gmail.com>
2020-02-03 21:35:30 -0500

    updated index to reflect filename change

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
-import MealCard from "./MealCard";
+import MealForm from "./MealForm";
 import GigapetCard from "./GigapetCard";
 import NavBar from "./NavBar";
 import Start from "./Start";
 
-export { Login, Register, Dashboard, MealCard, GigapetCard, NavBar, Start };
+export { Login, Register, Dashboard, MealForm, GigapetCard, NavBar, Start };
