486ea36c9688ace0875f4535769d917facf10f46
debauchery1st <trevor.j.martin@gmail.com>
2020-02-06 12:57:27 -0500

    dynamic title display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import {
   Login,
@@ -9,12 +9,37 @@ import {
   MealForm,
   MealList
 } from "./components";
-// import GigapetView from "./components/GigapetView";
-import PrivateRoute from "./components/PrivateRoute";
 
+import PrivateRoute from "./components/PrivateRoute";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 function App() {
+  const [title, setTitle] = useState("welcome");
   const location = useLocation();
-  const title = location.pathname.replace("/", "") || "welcome";
+  useEffect(() => {
+    console.log(location.pathname);
+    if (location.pathname === "/dashboard") {
+      setTitle("my gigapets");
+    } else if (location.pathname.includes("meal")) {
+      const pet_id = location.pathname.split("/").pop();
+      console.log(`the pet id = ${pet_id} `);
+      // axios /pets/:id
+      axiosWithAuth()
+        .get(`/pets/${pet_id}`)
+        .then(res => {
+          setTitle("welcome");
+          const petName = res.data.name;
+          if (location.pathname.includes("list")) {
+            setTitle(`${petName}'s meal list`);
+          } else {
+            setTitle(`feed ${petName}`);
+          }
+          // if (location.pathname.includes(""))
+          // location.pathname.includes("list")
+          // ? `${petName}'s meal list`
+          // : `feed ${petName}`
+        });
+    } else setTitle(location.pathname.split("/").pop());
+  }, [location.pathname]);
   return (
     <div className="App">
       <NavBar />
@@ -24,7 +49,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Start} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />        
+        <Route exact path="/register" component={Register} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/meal-list/:petId" component={MealList} />
         <PrivateRoute path="/meal/:petId" component={MealForm} />
